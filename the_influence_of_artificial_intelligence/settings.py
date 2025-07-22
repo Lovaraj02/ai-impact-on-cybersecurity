90a1b6efac62f154b00a95ba1e40fa4b04be8184
lovaraj02 <rajubandaru2255@gmail.com>
2025-07-22 12:02:08 +0530

    db changed

--- the_influence_of_artificial_intelligence/settings.py
+++ the_influence_of_artificial_intelligence/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Remote_User',
     'Service_Provider',
-    'livereload'
 ]
 
 
@@ -44,7 +43,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 
@@ -73,16 +71,25 @@ WSGI_APPLICATION = 'the_influence_of_artificial_intelligence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'NAME': 'the_influence_of_artificial_intelligence',
+#         'USER':'root',
+#         'PASSWORD': '',
+#         'HOST' :'127.0.0.1',
+#         'PORT' :'3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'the_influence_of_artificial_intelligence',
-        'USER':'root',
-        'PASSWORD': '',
-        'HOST' :'127.0.0.1',
-        'PORT' :'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 
@@ -136,7 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'Template/media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
